3470a385c1a58c903f7403cd0be872b791cc1ac1
KuldeepSonara <93315882+KuldeepSonara@users.noreply.github.com>
2022-08-03 18:13:39 +0530

    print hello world

--- main.java
+++ main.java
@@ -1,5 +1,6 @@
 public class main{
 	public static void main(String[] args) {
-		System.out.println("my village: Hamirpar \n tankara,morbi,gujarat \n pin-363650");
+		System.out.println("Hellow world");
 	}
 }
+	
