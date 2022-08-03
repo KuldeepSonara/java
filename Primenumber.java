7ee1267cc031894d75373a4103041c01385bdf1f
KuldeepSonara <93315882+KuldeepSonara@users.noreply.github.com>
2022-08-03 18:11:06 +0530

    get prime number to N

--- Primenumber.java
+++ Primenumber.java
@@ -6,21 +6,25 @@ public class Primenumber{
 		Scanner sc = new Scanner(System.in);
 		System.out.printf("Enter your number:: ");
 		int num=sc.nextInt();
-		boolean flag=true;
-		for (int i=2;i<num/2 ;i++ ) {
-			if(num%i==0){
-				flag=false;
-				break;
+		// boolean flag=true;
+		for (int i=2;i<=num ;i++ ) {
+			boolean flag=true;
+			for(int j=2;j<i;j++){
+				if(i%j==0){
+					flag=false;
+					break;
+				}
 			}
+			if(flag){
+				System.out.println(i+" is prime number");
+			}
+			// else{
+			// 	System.out.println(i+" is not prime number");
+			// }
 		}
-		if(flag){
-			System.out.println(num+" is prime number");
-		}
-		else{
-			System.out.println(num+" is not prime number");
-		}
+		
 
 
-	
-}
+
+	}
 }
